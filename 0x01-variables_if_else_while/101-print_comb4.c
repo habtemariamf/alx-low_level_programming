7b220ad05968f456c8eff75efdc7ec965fd349fd
habtemariamf <habte21fb@gmail.com>
2022-12-09 10:41:41 -0800

    aditional tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,24 +17,26 @@ int main(void)
 {
 	int a, b, c, d;
 
-		for (a = 0; a < 1000; a++)
+	for (a = 0; a < 1000; a++)
+	{
+	b = a / 100; /* hundreds */
+	c = (a / 10) % 10; /* tens */
+	d = a % 10; /* singles */
 
-				{
-		b = a / 100; /* hundreds */
-		c = (a / 10) % 10; /* tens */
-		d = a % 10; /* singles */
-		if (b < c && c < d)
-		{
-		putchar(b + '0');
-		putchar(c + '0');
-		putchar(d + '0');
-		if (a < 700)
-		{
-		putchar(',');
-		putchar(' ');
+	if (b < c && c < d)
+	{
+	putchar(b + '0');
+	putchar(c + '0');
+	putchar(d + '0');
+
+	if (a < 700)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
 		}
-		}
-				}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+
+	return (0);
 }
