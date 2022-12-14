e2908e585d29160810e49418a4851d534115c234
habtemariamf <habte21fb@gmail.com>
2022-12-13 21:12:18 -0800

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,24 +6,25 @@
 */
 int main(void)
 {
-	long double f1 = 1, f2 =1, f3;
-	int i;
+	int f1, f2, i, q, b;
 
 	i = 1;
+	f1 = 1;
+	f2 = 2;
+	a = 2;
+	b = 2;
 
-	printf("%.Lf, ", f2);
-	while (i <= 49)
+	while (i <= 30)
 	{
 		f3 = f1 + f2;
-		printf("%.Lf", f3);
 		i++;
 		f1 = f2;
 		f2 = f3;
-		if (i != 50)
+		if (f3 % b == 0)
 		{
-			printf((", ");
-					}
-					}
-					putchar('\n');
-					return (0);
-					}
+			a = a +f3;
+		}
+	}
+	printf("%d\n", a);
+	return (0);
+}
