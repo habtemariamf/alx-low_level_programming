38116632b5b2b0c6dba2602a226a0d48b27e670b
habtemariamf <habte21fb@gmail.com>
2022-12-18 23:59:03 -0800

    reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - resets value in address referenced by pointer.
- * @n: pointer.
- * Return: void.
- *  */
+* reset_to_98 - resets value in address referenced by pointer.
+* @n: pointer.
+* Return: void.
+*/
 void reset_to_98(int *n)
 {
 
