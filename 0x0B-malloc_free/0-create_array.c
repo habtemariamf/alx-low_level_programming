bad66661f9ed56aaf39c7170660395ba6e05d9a9
habtemariamf <habte21fb@gmail.com>
2023-01-09 02:37:12 -0800

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,6 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	s = malloc(size * sizeof(char));
-
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
