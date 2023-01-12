3c0057cffd5374cf15248af877fd89cd563594eb
Habtemariam <117764997+habtemariamf@users.noreply.github.com>
2023-01-12 16:09:52 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
-*malloc_checked - array for prints a string
-*@b: number of memory
-*Return: void
-*/
+ * malloc_checked - Entry Point
+ * @b: input amount
+ * Return: pointer to new mem
+ */
 void *malloc_checked(unsigned int b)
 {
-void *p;
+	void *n;
 
-p = malloc(b);
-if (p == NULL)
-{
-exit(98);
-}
-return (*p);
+	n = malloc(b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
 }
