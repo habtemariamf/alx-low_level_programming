cbb2f1b5ff3b39974cc724c965468480fbef6b60
habtemariamf <habte21fb@gmail.com>
2023-01-13 10:04:49 -0800

    more_malloc_free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * malloc_checked - Entry Point
- * @b: input amount
- * Return: pointer to new mem
- */
+* malloc_checked - Entry Point
+* @b: input amount
+* Return: pointer to new mem
+*/
 void *malloc_checked(unsigned int b)
 {
 void *mem = malloc(b);
 
+{
 if (mem == NULL)
 exit(98);
+}
 return (mem);
 }
