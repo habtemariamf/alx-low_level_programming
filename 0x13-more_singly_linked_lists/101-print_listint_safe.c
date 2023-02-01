6da11327099acf2bf17ed6b6ce1d84660fe8998c
Habtemariam <117764997+habtemariamf@users.noreply.github.com>
2023-02-01 10:40:26 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,9 +1,7 @@
 include "lists.h"
-
 /**
  * free_listp - frees a linked list
  * @head: head of a list.
- *
  * Return: no return.
  */
 void free_listp(listp_t **head)
@@ -26,7 +24,6 @@ void free_listp(listp_t **head)
 /**
  * print_listint_safe - prints a linked list.
  * @head: head of a list.
- *
  * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
